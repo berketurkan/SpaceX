caa7a951ad2ef8c377505072bb6fdef1e70fc9f5
berketurkan <berke.turkan@sabanciuniv.edu>
2024-08-12 17:22:32 +0300

    TASK DOBBY-33: SpaceX iOS:  Create SpaceX iOS project
    SOLUTION: SpaceX iOS Project is created.

---
+++ SpaceX iOS/SpaceX iOS/SpaceX_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceX_iOSApp.swift
+//  SpaceX iOS
+//
+//  Created by Vestel on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceX_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
