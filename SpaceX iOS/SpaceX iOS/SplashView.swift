ecef83d321afbf3905c458b9eefdd8dd951ddf4c
berketurkan <berke.turkan@sabanciuniv.edu>
2024-08-13 11:05:54 +0300

    TASK DOBBY-34: Project: Create splash screen
    SOLUTION: Splash Screen is created.

---
+++ SpaceX iOS/SpaceX iOS/SplashView.swift
@@ -0,0 +1,39 @@
+//
+//  SplashView.swift
+//  SpaceX iOS
+//
+//  Created by Vestel on 13.08.2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if self.isActive {
+                SpaceXTabView()
+            } else {
+                Image("SpaceXSplashScreen")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
