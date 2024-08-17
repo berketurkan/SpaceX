d8af004698dbb1ededace84f3483291a56d7f029
berketurkan <168541122+berketurkan@users.noreply.github.com>
2024-08-17 12:20:56 +0300

    TASK DOBBY-38: Create Rocket Model
    SOLUTION: Rocket Model is added to project.

--- SpaceX iOS/SpaceX iOS/Model/Rocket.swift
+++ SpaceX iOS/SpaceX iOS/Model/Rocket.swift
@@ -5,4 +5,50 @@
 //  Created by Vestel on 13.08.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct Rocket: Decodable, Identifiable {
+    
+    let id: Int
+    let name: String
+    let description: String
+    let imageURL: String
+    var isFavorite = false
+    let heightMeter: Float
+    let heightFeet: Float
+    let diameterMeter: Float
+    let diameterFeet: Float
+    let massKg: Float
+    let massLb: Float
+    let images: [String]
+    let payloadWeights: [Payload]
+    
+}
+
+struct Payload: Decodable,Identifiable {
+    let id: Int
+    let name: String
+    let kg: Int
+    let lb: Int
+}
+
+struct RocketResponse: Decodable {
+    let request: [Rocket]
+}
+
+struct MockData {
+    static let sampleRocket = Rocket(id: 001,
+                                     name: "Test Rocket",
+                                     description: "Amazing",
+                                     imageURL: "TestRocket",
+                                     isFavorite: false,
+                                     heightMeter: 111,
+                                     heightFeet: 111,
+                                     diameterMeter: 111,
+                                     diameterFeet: 111,
+                                     massKg: 111,
+                                     massLb: 111,
+                                     images: ["TestRocket","TestRocket"],
+                                     payloadWeights: [])
+    static let sampleRockets = [sampleRocket,sampleRocket,sampleRocket,sampleRocket]
+}
